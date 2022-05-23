e19f3cacef83260366ad809074a228cd87837b0f
Halil <78084765+halilkaran@users.noreply.github.com>
2022-05-23 12:07:52 +0200

    first commit

---
+++ products/admin.py
@@ -0,0 +1,98 @@
+
+from django.contrib import admin
+from .models import Product, Review, Category
+from django.utils import timezone
+from django.utils.safestring import mark_safe
+
+
+
+class ReviewInline(admin.TabularInline):  # StackedInline farklı bir görünüm aynı iş
+    '''Tabular Inline View for '''
+    model = Review
+    extra = 1
+    classes = ('collapse',)
+    # min_num = 3
+    # max_num = 20
+
+
+ 
+  
+
+
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "create_date", "is_in_stock", "update_date", "added_days_ago", "how_many_reviews", "bring_img_to_list")     
+    # list_display_links = ("create_date", ) #can't add items in list_editable to here
+    list_filter = ("is_in_stock", "create_date")
+    ordering = ("name",)  
+    search_fields = ("name",)
+    prepopulated_fields = {'slug' : ('name',)}   # when adding product in admin site
+    list_per_page = 25
+    date_hierarchy = "update_date"
+    # fields = (('name', 'slug'), 'description', "is_in_stock") #fieldset kullandığımız zaman bunu kullanamayız
+    inlines = (ReviewInline,)
+    list_editable = ( "is_in_stock", )
+
+    fieldsets = (
+        (None, {
+            "fields": (
+                ('name', 'slug'), "is_in_stock" # to display multiple fields on the same line, wrap those fields in their own tuple
+            ),
+            # 'classes': ('wide', 'extrapretty'), wide or collapse
+        }),
+        ('Optionals Settings', {
+            "classes" : ("collapse", ),
+            "fields" : ("description", "categories","product_img", "bring_image"),
+            'description' : "You can use this section for optionals settings"
+        })
+    )
+    filter_horizontal = ("categories", )
+    readonly_fields = ("bring_image",)
+
+
+
+    actions = ("is_in_stock", )
+
+    def is_in_stock(self, request, queryset):
+            count = queryset.update(is_in_stock=True)
+            self.message_user(request, f"{count} çeşit ürün stoğa eklendi")
+        
+    is_in_stock.short_description = 'İşaretlenen ürünleri stoğa ekle'
+
+
+
+
+    def added_days_ago(self, product):
+        fark = timezone.now() - product.create_date
+        return fark.days
+    
+    def bring_img_to_list(self, obj):
+        if obj.product_img:
+            return mark_safe(f"<img src={obj.product_img.url} width=50 height=50></img>")
+        return mark_safe("******")
+    bring_img_to_list.short_description = "product_image"
+
+
+
+    
+
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+ 
+
+admin.site.site_title = "Clarusway Title"
+admin.site.site_header = "Clarusway Admin Portal"  
+admin.site.index_title = "Welcome to Clarusway Admin Portal"
+actions = ("is_in_stock", )
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'created_date', 'is_released')
+    list_per_page = 50
+    raw_id_fields = ('product',) 
+
+admin.site.register(Review, ReviewAdmin)
